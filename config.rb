8793631f6c1e80b0780b61f17f565107dc31c5b0
Anthony Torfs <torfs.anthony@hotmail.com>
2016-03-14 23:10:42 +0100

    f

--- config.rb
+++ config.rb
@@ -30,6 +30,10 @@ activate :autoprefixer
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
+  
+data.flats.each do |owner|
+  proxy "/flats/#{owner[0]}.html", "/flats/show.html", :locals => { :owner => owner[0] }, :ignore => true
+end
 
 ###
 # Helpers
